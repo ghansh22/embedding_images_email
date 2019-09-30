4b3d49834b23973a13fb268bd27ae4026db95421
Ghanshyam Baviskar <gnbaviskar2@gmail.com>
2019-09-30 09:49:21 +0530

    proper var names

--- embedded_html_image.py
+++ embedded_html_image.py
@@ -18,35 +18,35 @@ try:
     email_conn.login(username, password)
 
 
-    msgRoot = MIMEMultipart("related")
-    msgRoot["Subject"] = "Sending embedded image in HTML using python2"
-    msgRoot["From"] = from_email
+    msg_root = MIMEMultipart("related")
+    msg_root["Subject"] = "Sending embedded image in HTML using python2"
+    msg_root["From"] = from_email
 
 
-    msgAlternative = MIMEMultipart('alternative')
-    msgRoot.attach(msgAlternative)
+    msg_alternative = MIMEMultipart('alternative')
+    msg_root.attach(msg_alternative)
 
-    msgText = MIMEText('This is the alternative plain text message.')
-    msgAlternative.attach(msgText)
+    msg_text = MIMEText('This is the alternative plain text message.')
+    msg_alternative.attach(msg_text)
 
     # We reference the image in the IMG SRC attribute by the ID we give it below
-    msgText = MIMEText('<b>Embedded <i>HTML</i> text</b> and an image.<br><img src="cid:pepipost_img2"><br/><br/>I am pepi. The superhero behind the scenes. My special power is inbox delivery...', 'html')
-    msgAlternative.attach(msgText)
+    msg_text = MIMEText('<b>Embedded <i>HTML</i> text</b> and an image.<br><img src="cid:pepipost_img2"><br/><br/>I am pepi. The superhero behind the scenes. My special power is inbox delivery...', 'html')
+    msg_alternative.attach(msg_text)
 
 
     # This example assumes the image is in the current directory
-    fp = open("image1.j", 'rb')
-    msgImage = MIMEImage(fp.read())
+    fp = open("image1.jpg", 'rb')
+    msg_image = MIMEImage(fp.read())
     fp.close()
 
 
     # Define the image's ID as referenced above
-    msgImage.add_header('Content-ID', '<pepipost_img2>')
-    msgRoot.attach(msgImage)
+    msg_image.add_header('Content-ID', '<pepipost_img2>')
+    msg_root.attach(msg_image)
 
     # print(msg.as_string())
 
-    email_conn.sendmail(from_email,to_list,msgRoot.as_string())
+    email_conn.sendmail(from_email,to_list,msg_root.as_string())
     email_conn.quit()
 
 except smtplib.SMTPException as e:
